92485e8a9d9be3dd0310644c61b3da25b6b865be
Yinli <jiangyinli0803@gmail.com>
2025-05-18 19:25:44 +0200

    update search component

--- backend/src/main/java/org/example/backend/AppConfig.java
+++ backend/src/main/java/org/example/backend/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
-public class Appconfig {
+public class AppConfig {
     @Value("${OpenAi_API_Key}")
     private String openAiApiKey;
 
--- backend/src/main/java/org/example/backend/controller/AiRecipeController.java
+++ backend/src/main/java/org/example/backend/controller/AiRecipeController.java
@@ -5,9 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.backend.model.chatgpt.AiRecipe;
 import org.example.backend.model.chatgpt.ChatgptRequest;
 import org.example.backend.model.chatgpt.ChatgptResponse;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClient;
 
